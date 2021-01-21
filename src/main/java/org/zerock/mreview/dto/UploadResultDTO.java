5a2383c524cca77d96b0ba4eddc4a725e8acc8eb
eundonglee <cr3306@naver.com>
2021-01-22 00:47:15 +0900

    feat: 업로드 처리 결과를 브라우저에 JSON으로 전달하도록 DTO 클래스 구성 및 컨트롤러 계층 기능 구현

---
+++ src/main/java/org/zerock/mreview/dto/UploadResultDTO.java
@@ -0,0 +1,25 @@
+package org.zerock.mreview.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+@Data
+@AllArgsConstructor
+public class UploadResultDTO {
+
+    private String fileName;
+    private String uuid;
+    private String forderPath;
+
+    public String getImageURL() {
+        try {
+            return URLEncoder.encode(forderPath + "/" + uuid + "_" + fileName, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
